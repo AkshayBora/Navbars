936c278815d79afdd775ef1abeec9ca1fe5dca60
AkshayBora <61018115+AkshayBora@users.noreply.github.com>
2023-08-08 00:57:50 +0530

    My responsive Navbars

---
+++ Navbar1/script.js
@@ -0,0 +1,6 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active")
+}
+// https://github.com/khan4019/front-end-Interview-Questions
